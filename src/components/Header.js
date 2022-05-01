d00fcf05ec94542e3c169d810eaca3d07d48e7d2
noman26636 <83615163+noman26636@users.noreply.github.com>
2022-05-01 18:32:39 +0500

    Left header elements

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import {SearchIcon,UserGroupIcon,HeartIcon,PaperAirplaneIcon,MenuIcon} from "@heroicons/react/outline"
+import { HomeIcon } from "@heroicons/react/solid"
+
+function Header() {
+
+
+
+    return (
+      <div className="shadow-md border-b bg-white sticky top-0 p-4 z-50 ">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto ">
+        
+
+          {/*LeftSide*/} 
+          
+            <div className='relative hidden lg:inline-grid  w-24 cursor-pointer items-center justify-center'>
+
+              <a href="/">
+              <img 
+                 className="flex items-center justify-center top-5 object-contain"
+                 alt="profileinsta"
+              src='https://links.papareact.com/ocw' 
+                    layout="fill"                  
+              />
+               </a>
+            </div>
+            </div>
+
+               </div>
